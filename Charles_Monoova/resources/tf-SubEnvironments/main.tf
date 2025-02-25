efd46c2a451a4f3ec7c7c22c0629718591657d9c
Charles Law <68914861+claw-ai@users.noreply.github.com>
2025-02-25 16:50:30 +1100

    uodated

--- Charles_Monoova/resources/tf-SubEnvironments/main.tf
+++ Charles_Monoova/resources/tf-SubEnvironments/main.tf
@@ -20,25 +20,25 @@ locals {
     environment_staging     = "Postgres Staging"
     group_other             = "Other"
     crm_masked              = "crm-mask"
-    m5_1A                   = "M5_1A"   
-    m5_1B                   = "M5_1B"   
-    m5_1C                   = "M5_1C"   
+    m5_2A                   = "M5_2A"   
+    m5_2B                   = "M5_2B"   
+    m5_2C                   = "M5_2C"   
 }
 
 
 
 ## M5.1A
-resource "delphix_vdb" "M5_1A" {
-    name                    = "M5_1A"
+resource "delphix_vdb" "M5_2A" {
+    name                    = "M5_2A"
     source_data_id          = local.crm_masked
     environment_id          = local.environment_staging
     environment_user_id     = "postgres"
     target_group_id         = local.group_other
-    database_name           = "M5_1A"
+    database_name           = "M5_2A"
     auto_select_repository  = true
     appdata_source_params = jsonencode({
-        mountLocation       = "/mnt/provision/monoova/M5_1A"
-        postgresPort        = 8060
+        mountLocation       = "/mnt/provision/monoova/M5_2A"
+        postgresPort        = 8065
     })
 
     configure_clone {
@@ -60,18 +60,18 @@ resource "delphix_vdb" "M5_1A" {
 }
 
 ## M5.1B
-resource "delphix_vdb" "M5_1B" {
-    depends_on              = [ delphix_vdb.M5_1A ]
-    name                    = "M5_1B"
-    source_data_id          = local.m5_1A
+resource "delphix_vdb" "M5_2B" {
+    depends_on              = [ delphix_vdb.M5_2A ]
+    name                    = "M5_2B"
+    source_data_id          = local.m5_2A
     environment_id          = local.environment_staging
     environment_user_id     = "postgres"
     target_group_id         = local.group_other
-    database_name           = "M5_1B"
+    database_name           = "M5_2B"
     auto_select_repository  = true
     appdata_source_params = jsonencode({
-        mountLocation       = "/mnt/provision/monoova/M5_1B"
-        postgresPort        = 8061
+        mountLocation       = "/mnt/provision/monoova/M5_2B"
+        postgresPort        = 8066
     })
 
     configure_clone {
@@ -93,18 +93,18 @@ resource "delphix_vdb" "M5_1B" {
 }
 
 ## M5.1C
-resource "delphix_vdb" "M5_1C" {
-    depends_on              = [ delphix_vdb.M5_1B ]
-    name                    = "M5_1C"
-    source_data_id          = local.m5_1B
+resource "delphix_vdb" "M5_2C" {
+    depends_on              = [ delphix_vdb.M5_2B ]
+    name                    = "M5_2C"
+    source_data_id          = local.m5_2B
     environment_id          = local.environment_staging
     environment_user_id     = "postgres"
     target_group_id         = local.group_other
-    database_name           = "M5_1C"
+    database_name           = "M5_2C"
     auto_select_repository  = true
     appdata_source_params = jsonencode({
-        mountLocation       = "/mnt/provision/monoova/M5_1C"
-        postgresPort        = 8062
+        mountLocation       = "/mnt/provision/monoova/M5_2C"
+        postgresPort        = 8067
     })
 
     configure_clone {
